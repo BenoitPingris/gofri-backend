5ce709d4ca6728a58be0705cbd4eaff52250ebd3
Benoit Pingris <benoit.pingris@pm.me>
2021-02-02 20:28:16 +0100

    feat: add new class to replace tortoise.Model: app.models.Base, it provides a get_or_404

---
+++ app/models/base.py
@@ -0,0 +1,15 @@
+from typing import Any, Type, Union
+from fastapi.exceptions import HTTPException
+from starlette import status
+from tortoise.models import Model
+from tortoise.query_utils import Q
+
+
+class Base(Model):
+    @classmethod
+    async def get_or_404(cls: Type[Model], *args: Q,
+                         **kwargs: Any) -> Union[Model, None]:
+        data = await cls.get_or_none(*args, **kwargs)
+        if not data:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+        return data
